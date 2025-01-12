7c74574aa53e232e767b956982ba06b8b2453a1e
mrangjw <157506327+mrangjw@users.noreply.github.com>
2025-01-12 18:16:44 +0900

    feat: Create Navbar

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,23 @@
+import styled from '@emotion/styled';
+import Navbar from '../../components/Navbar';
+
+const HomeContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background: white;
+`;
+
+const Content = styled.div`
+  padding-top: 80px; // Navbar 높이만큼 패딩
+`;
+
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <HomeContainer>
+      <Navbar />
+      <Content>{/* 여기에 홈페이지의 다른 내용이 들어갈 예정 */}</Content>
+    </HomeContainer>
+  );
 };
 
 export default Home;
