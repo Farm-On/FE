7a963ca19bb70bf98657c925911ed810bb54f449
YH <imrubi5555@gmail.com>
2025-01-26 18:44:18 +0900

    feat: 전문자 내 프로필 디자인 구현

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import PretendardMedium from '@/assets/fonts/Pretendard-Medium.woff';
 // 페이지
 import Home from './pages/Home';
 import MyEstimatePage from './pages/Home/MyEstimate';
+import My from './pages/Expert/My';
 
 const AppContainer = styled.div`
   width: 100%;
@@ -43,6 +44,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/MyEstimate" element={<MyEstimatePage />} />
+          <Route path="/expert/my" element={<My />} />
         </Routes>
       </AppContainer>
     </Router>
