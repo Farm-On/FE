dbe5824b5432c02b232edc4b84dc8e488d186b10
Daeuni <ekdmsjj@gmail.com>
2025-01-30 03:31:23 +0900

    feat: 커뮤니티 검색바 컴포넌트

---
+++ src/components/CommunitySearch.tsx
@@ -0,0 +1,48 @@
+import styled from "@emotion/styled";
+import SearchIcon from '../assets/icons/search.svg?react';
+
+
+interface CSProps{
+    onChange:(e: React.ChangeEvent<HTMLInputElement>)=>void;
+    value:string;
+}
+
+export const CommunitySearch = ({onChange,value}:CSProps)=>{
+    return(
+        <div style={{paddingLeft: "11.4vw", paddingTop: '17.5vw'}}>
+            <div style={{position:'relative'}}>
+                <Container
+                placeholder='찾고싶은 글을 검색하세요'
+                onChange={onChange}
+                value={value}
+                type="text"
+                >
+                </Container>
+                <StyledIcon />
+            </div>
+        </div>
+        )
+
+}
+
+const Container = styled.input`
+width: 876px;
+height: 66px;
+flex-shrink: 0;
+border-radius: 18px;
+background: #EEE;
+border:none;
+padding-left:80px;
+color: var(--gray-400, #8E8E8E);
+font-family: 'PretendardRegular';
+font-size: 18px;
+line-height: 166%; /* 29.88px */
+text-transform: uppercase;
+`
+const StyledIcon = styled(SearchIcon)`
+width: 32px;
+height: 32px;
+position:absolute;
+left:30px;
+top:17px;
+`
