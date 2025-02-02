b8f35311d13ccfc704cf2af58e96ddcbb811faee
soohee Jang <shjang0820@hansung.ac.kr>
2025-02-02 15:52:13 +0900

    feat : x icon 이름 수정

--- src/pages/ExpertRegistration/Location.tsx
+++ src/pages/ExpertRegistration/Location.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as L from '@/styles/pages/ExpertRegistration/Location.style';
-import XIcon from '@/assets/icons/x.svg?react';
+import XIcon from '@/assets/icons/greenX.svg';
 
 type LocationData = {
   [key: string]: string[];
