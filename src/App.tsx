0d442167bc931152f02c938e6aec96b4f56b9e4c
mrangjw <157506327+mrangjw@users.noreply.github.com>
2025-02-02 09:03:07 +0900

    chore: 채팅 기능 구현을 위한 의존성 및 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import styled from '@emotion/styled';
 import { css, Global } from '@emotion/react';
 
@@ -11,11 +11,37 @@ import PretendardMedium from '@/assets/fonts/Pretendard-Medium.woff';
 import Home from './pages/Home';
 import MyEstimatePage from './pages/Home/MyEstimate';
 import RequestEstimatePage from './pages/Home/RequestEstimate';
+import ChatList from './pages/chat/ChatList';
+import ChatRoom from './pages/chat/ChatRoom';
+
+// 컴포넌트
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
 
 const AppContainer = styled.div`
   width: 100%;
   min-height: 100vh;
 `;
+
+// 레이아웃 컴포넌트
+const AppRoutes = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
+  return (
+    <>
+      {!isHomePage && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/MyEstimate" element={<MyEstimatePage />} />
+        <Route path="/MyEstimate/RequestEstimate" element={<RequestEstimatePage />} />
+        <Route path="/chat" element={<ChatList />} />
+        <Route path="/chat/:roomId" element={<ChatRoom />} />
+      </Routes>
+      {!isHomePage && <Footer />}
+    </>
+  );
+};
 
 function App() {
   return (
@@ -28,7 +54,6 @@ function App() {
               src: url(${PretendardRegular}) format('woff');
               font-weight: 500;
             }
-
             @font-face {
               font-family: 'PretendardSemiBold';
               src: url(${PretendardSemiBold}) format('woff');
@@ -39,13 +64,13 @@ function App() {
               src: url(${PretendardMedium}) format('woff');
               font-weight: 500;
             }
+            body {
+              margin: 0;
+              padding: 0;
+            }
           `}
         />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/MyEstimate" element={<MyEstimatePage />} />
-          <Route path="/MyEstimate/RequestEstimate" element={<RequestEstimatePage />} />
-        </Routes>
+        <AppRoutes />
       </AppContainer>
     </Router>
   );
