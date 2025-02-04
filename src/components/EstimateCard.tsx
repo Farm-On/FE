62f0835ee9169bef96bc169160e92828be61a760
Daeuni <ekdmsjj@gmail.com>
2025-02-05 00:13:08 +0900

    style: 스타일컴포넌트 파일로 이동

--- src/components/EstimateCard.tsx
+++ src/components/EstimateCard.tsx
@@ -1,5 +1,4 @@
 import * as EC from '@/styles/components/EstimateCard.style';
-import styled from '@emotion/styled';
 
 interface CardProps {
   title: string;
@@ -21,7 +20,7 @@ export const EstimateCard = ({
   money,
 }: CardProps) => {
   return (
-    <Container>
+    <EC.Container>
       <EC.Card>
         <EC.Content>
           <h2>{title}</h2>
@@ -29,79 +28,14 @@ export const EstimateCard = ({
             {productName}
             {`(${product})`} | {category} | {region}
           </h3>
-          <ContentLine />
-          <Bottom>
+          <EC.ContentLine />
+          <EC.Bottom>
             <h4>₩ {money}</h4>
             <p>{date}</p>
-          </Bottom>
+          </EC.Bottom>
         </EC.Content>
       </EC.Card>
-    </Container>
+    </EC.Container>
   );
 };
 
-const Container = styled.div`
-  width: 390px;
-  height: 149px;
-
-  @media (max-width: 768px) {
-    width: 150px;
-    height: 120px;
-  }
-
-  @media (max-width: 480px) {
-    width: 100px;
-    height: 98px;
-  }
-`;
-const ContentLine = styled.div`
-  width: 337px;
-  height: 1px;
-  background-color: #c1c1c1;
-
-  
-    @media (max-width: 768px) {
-      width: 150px;
-      display:none;
-    }
-
-    @media (max-width: 480px) {
-      width: 130px;
-      display:none;
-    }
-`;
-const Bottom = styled.div`
-  display: flex;
-  justify-content: space-between;
-  height:21px;
-  
-  h4 {
-    margin-top: 20px;
-    margin: 0;
-    font-size: 14px;
-    color: #666;
-    padding-left:;
-    font-family: 'Pretendard';
-    height:21px;
-    padding-top:5px;
-  }
-  p {
-    color: #8E8E8E;
-    font-family: 'Pretendard';
-    font-size: 12px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: 150%; /* 18px */
-    margin:0;
-    padding-top:5px;
-  }
-
-
-      @media (max-width: 768px) {
-      display:none;
-    }
-
-    @media (max-width: 480px) {
-      display:none;
-    }
-`;
--- src/styles/components/EstimateCard.style.ts
+++ src/styles/components/EstimateCard.style.ts
@@ -81,3 +81,68 @@ export const Content = styled.div`
   }
 
 `;
+export const Container = styled.div`
+  width: 390px;
+  height: 149px;
+
+  @media (max-width: 768px) {
+    width: 150px;
+    height: 120px;
+  }
+
+  @media (max-width: 480px) {
+    width: 100px;
+    height: 98px;
+  }
+`;
+export const ContentLine = styled.div`
+  width: 337px;
+  height: 1px;
+  background-color: #c1c1c1;
+
+  
+    @media (max-width: 768px) {
+      width: 150px;
+      display:none;
+    }
+
+    @media (max-width: 480px) {
+      width: 130px;
+      display:none;
+    }
+`;
+export const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height:21px;
+  
+  h4 {
+    margin-top: 20px;
+    margin: 0;
+    font-size: 14px;
+    color: #666;
+    padding-left:;
+    font-family: 'Pretendard';
+    height:21px;
+    padding-top:5px;
+  }
+  p {
+    color: #8E8E8E;
+    font-family: 'Pretendard';
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 150%; /* 18px */
+    margin:0;
+    padding-top:5px;
+  }
+
+
+      @media (max-width: 768px) {
+      display:none;
+    }
+
+    @media (max-width: 480px) {
+      display:none;
+    }
+`;
